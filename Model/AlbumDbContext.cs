58a6133814f50e3578f41cbbe16e66b06e3c76cf
Eduardo <hitogoroshi@outlook.com>
2017-12-21 12:25:12 -0500

    asp.net net core project

---
+++ Model/AlbumDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Domain;
+
+namespace Model
+{
+    public class AlbumDbContext : DbContext
+    {
+        public AlbumDbContext(DbContextOptions<AlbumDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Album { get; set; }
+    }
+}
---
+++ Model/Migrations/20171221171211_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Model.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Album",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    PhotoLink = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Album", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Album");
+        }
+    }
+}
